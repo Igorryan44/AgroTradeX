0801b1824df75f2ecfb0781a228dc4d12371f858
Igorryan44 <igorryanmendesalves@gmail.com>
2026-01-15 18:57:37 -0300

    Iniciando projeto AgroTradeX

---
+++ src/main/java/com/dev/igor/AgroTradeX/AgroTradeXApplication.java
@@ -0,0 +1,13 @@
+package com.dev.igor.AgroTradeX;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgroTradeXApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgroTradeXApplication.class, args);
+	}
+
+}
